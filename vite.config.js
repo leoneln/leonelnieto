be0c4cf11e1e90d00e2520ec3eaa39adb271fbb2
Leonel Nieto <leonelnieto@mac.lan>
2026-05-14 22:11:48 -0600

    base path shoudl be just /

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // If deploying to a subdirectory (e.g. GitHub Pages), set base:
-  base: '/leonelnieto/',
+  base: '/',
 })
