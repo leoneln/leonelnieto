2274dad657cc6321338b4461aee38d05e4b9e098
Leonel Nieto <leonelnieto@mac.lan>
2026-05-14 21:58:02 -0600

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // If deploying to a subdirectory (e.g. GitHub Pages), set base:
-  // base: '/leonieto-website/',
-  base: '/',
+  base: '/leonelnieto/',
 })
